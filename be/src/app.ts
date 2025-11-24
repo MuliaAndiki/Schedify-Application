0ee33cbf72230c1529bab3913bde541af99e9fa1
Mulia Andiki <muliaandiki@gmail.com>
2025-11-24 17:23:00 +0700

    feat:app be

--- be/src/app.ts
+++ be/src/app.ts
@@ -16,7 +16,14 @@ class App {
     this.app.get("/", () => "Hello Elysia! Bun js");
   }
   private middlewares() {
-    this.app.use(cors({ origin: "*" }));
+    this.app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+      })
+    );
     this.app.group("/api", (api) =>
       api.use(authRoutes).use(categoryRoutes).use(taskRoutes)
     );
